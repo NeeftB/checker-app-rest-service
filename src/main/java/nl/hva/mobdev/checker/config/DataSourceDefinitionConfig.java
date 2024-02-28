ee528f9f479ef91081ead1f9818a776b571de190
Brady Neeft <54938504+NeeftB@users.noreply.github.com>
2024-02-28 13:12:51 +0100

    Update DataSourceDefinitionConfig.java

--- src/main/java/nl/hva/mobdev/checker/config/DataSourceDefinitionConfig.java
+++ src/main/java/nl/hva/mobdev/checker/config/DataSourceDefinitionConfig.java
@@ -15,7 +15,7 @@ import javax.ejb.Stateless;
 //        url = "jdbc:mysql://localhost:3306/checker?createDatabaseIfNotExist=true&serverTimezone=CET",
         url = "jdbc:mysql://localhost:3306/checker?createDatabaseIfNotExist=true&serverTimezone=CET&useSSL=false&allowPublicKeyRetrieval=true",
         user = "root", //fill in your username of your MySQL server.
-        password = "Dr!ger13" //fill in your password of your MySQL server.
+        password = "Password" //fill in your password of your MySQL server.
 )
 
 
